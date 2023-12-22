87b6b5244d7c7d2c5e275b72c92639dbbafd2c56
Georges <georges.ltaif@gmail.com>
2023-12-22 15:37:30 +0200

    Commit

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
 from sqlalchemy.orm import relationship
 from forms import CreatePostForm, RegisterForm, LoginForm, CommentForm
+
+import os
 # Optional: add contact me email functionality (Day 60)
 # import smtplib
-
 
 '''
 Make sure the required packages are installed: 
@@ -23,9 +24,8 @@ pip3 install -r requirements.txt
 This will install the packages from the requirements.txt for this project.
 '''
 
-
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("app_config")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -50,7 +50,7 @@ gravatar = Gravatar(app,
                     base_url=None)
 
 # CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy()
 db.init_app(app)
 
